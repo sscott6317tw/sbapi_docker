ad214f438c8d212b547d27db3cb20c85a9182337
SUNSYS\jinqun.lin <jinqun.lin@nextfortune.tw>
2022-01-11 13:50:57 +0800

    20210111 Mobile Betting 修改

--- mobile_betting.py
+++ mobile_betting.py
@@ -37,6 +37,7 @@ class mobile_api_betting:
             
             retry = 0 
             while retry < 3:
+                print(self.id)
                 mobile_api = Api_Object.Login().login_api(device='app', user=self.id,password=self.pw, url=self.url,central_account=self.central_account,central_password=self.central_password)
                 if "nova88" in self.url and mobile_api == False:
                     time.sleep(60)
@@ -46,8 +47,8 @@ class mobile_api_betting:
             mobile_api.set_odds_type(odds_type=str(odds_type))
         
             already_list = []
-            bet_type_list = ['OU','more']
-            #bet_type_list = ['more']
+            #bet_type_list = ['OU','more']
+            bet_type_list = ['more']
             for bet_type in bet_type_list:
                 print(self.match_id)
                 showallodds_result = mobile_api.ShowAllOdds(type='test',market=str(self.market[0]).lower(),filter=self.match_id,sport=self.sport,bet_type=bet_type)
@@ -83,7 +84,7 @@ class mobile_api_betting:
         print("FAIL Test Case : %s "%fail_count)
         
 
-'''
+
 url = sys.argv[1]
 id=sys.argv[2]
 pw=sys.argv[3]
@@ -95,14 +96,8 @@ match_id=sys.argv[8]
 all_bet_choice=sys.argv[9]
 all_odds_type=sys.argv[10]
 all_odds_type = all_odds_type.split(',')
-'''
 
-
-
-
-
-#mobile_api_betting_test = mobile_api_betting(url= 'http://smart.athena000.com/',id='twqa10',pw='1q2w3e4r',central_account='web.desktop',central_password='1q2w3e4r',market='Early',sport="Soccer",all_bet_choice='true',match_id='49301197',all_odds_type=["CN"])
-#mobile_api_betting_test = mobile_api_betting(url= 'http://qawer.nova88.com:5182/',id='wendy01',pw='111111',central_account='web.desktop',central_password='1q2w3e4r',market='Early',sport="Cricket",all_bet_choice='true',match_id='49349092',all_odds_type=["Dec"])
-mobile_api_betting_test = mobile_api_betting(url= 'http://qawer.nova88.com:5303/',id='twpgmwendyliu02',pw='111111',central_account='web.desktop',central_password='1q2w3e4r',market='Live',sport="E-Sports",all_bet_choice='true',match_id='49556210',all_odds_type=["MY"])
-
+#mobile_api_betting_test = mobile_api_betting(url= 'http://smart.athena000.com/',id='autoqa01',pw='1q2w3e4r',central_account='web.desktop',central_password='1q2w3e4r',market='Early',sport="Soccer",all_bet_choice='true',match_id='49573658',all_odds_type=["MY"])
+#mobile_api_betting_test = mobile_api_betting(url= 'http://qawer.nova88.com:5303/',id='twpgmwendyliu02',pw='111111',central_account='web.desktop',central_password='1q2w3e4r',market='Early',sport="E-Sports",all_bet_choice='true',match_id='49552806',all_odds_type=["MY"])
+mobile_api_betting_test = mobile_api_betting(url= url,id=id,pw=pw,central_account=central_account,central_password=central_password,market=market,sport=sport,all_bet_choice=all_bet_choice,match_id=match_id,all_odds_type=all_odds_type)
 mobile_api_betting_test.api_betting()

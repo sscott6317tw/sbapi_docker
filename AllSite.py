0d471083bc71d5baa49e42a0a3e12ced9df8dea8
kerr <kerr.hsieh@nextfortune.tw>
2022-01-13 17:20:47 +0800

    增加時間顯示

--- AllSite.py
+++ AllSite.py
@@ -1,6 +1,6 @@
 
 #In[]
-import Api_Object
+import Api_Object,  datetime , time
 from collections import defaultdict
 from Common import Env
 from  Logger import create_logger 
@@ -244,16 +244,17 @@ site_list = list(Env().api_url_dict['mobile'].keys())
 #site_list = ['Xtu168']
 site_api_test = Site_Api()
 #In[]
-
-
+time_start = time.time() #開始計時 
+print('開始執行時間 : %s'%datetime.datetime.now().strftime('%Y-%m-%d/%H:%M:%S') )
 
 for site in site_list:
     log.info('site : %s'%site)
     site_api_test.site_api_betting_process(site = site  )# 執行整個 case流程
     site_api_test.Api_Status(site)# 回傳 該site 的 staus邏輯
 
+time_end = time.time()
 
-
+print('完成時間 : ',  time_end - time_start,'s')
 #log.info('all site : %s'%site_api_test.site_dict)
 log.info('letstalk : %s'%site_api_test.lets_talk)
 
